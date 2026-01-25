71a3516b0c33d63ee207c95772aba4c6c7355193
meloooong <hhho0coco@gmail.com>
2026-01-26 02:45:44 +0900

    python day12 if문 심화, 일괄처리(리팩토링), 대입 연산자 개념

---
+++ test12.py
@@ -0,0 +1,25 @@
+# if 문
+
+if 1 > 2:
+    print("큰 값 : " + str(1))
+elif 2 > 1:
+    print("큰 값 : " + str(2))
+else:
+    print("==")
+
+# 일괄처리(효율적 + 가독성) = 리팩토링
+
+result = ""
+
+# if 문장
+    # result = 변수
+# elif 문장
+    # result = 변수
+
+# print(result)
+
+# python : switch 문은 존재하지 않는다!
+
+# 대입 연산자(복합 대입 연산자, 누적 연산자)
+# += -= *= /= %= //=
+
