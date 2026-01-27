b6a06983e01f77b8b3236211fd44c05217da2d16
meloooong <hhho0coco@gmail.com>
2026-01-27 23:54:46 +0900

    python day21 list, dict 심화 문제

---
+++ test21.py
@@ -0,0 +1,8 @@
+numDict = {"even" : [2, 4, 6]}
+for i in numDict["even"]:
+    print(i)
+
+numListDict = {"1학년" : [[3, 4, 5], [7, 8, 9]]}
+for i in numListDict["1학년"]:
+    for j in i:
+        print(j)
